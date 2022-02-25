296e92a45f85b45ba4ab1db446e601cdae43c3e2
ooophantom421 <ooophantom421@gmail.com>
2022-02-25 22:17:55 +0900

    commit.

---
+++ smart_contract/test/betting_contract.js
@@ -0,0 +1,28 @@
+const BettingContract = artifacts.require("BettingContract");
+const fs = require('fs');
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract('BettingContract', async accounts => {
+  it("send ETH to Contract", async () => {
+    // token contract deploy
+    let deployed_betting_contract = await BettingContract.deployed();
+
+    // send ETH to Contract from fifth account
+    address(deployed_betting_contract.address).transfer (10 * 10 ** 18, {from: accounts[5]});
+  });
+
+  it("multiple betting", async () => {
+    // token contract deploy
+    let deployed_betting_contract = await BettingContract.deployed();
+
+    // betting 10 times
+    for (var i = 0; i < 10; i ++) {
+      var ret = await deployed_betting_contract.bettngNow(i % 2, {from: accounts[0], value: 10 * 10 ** 15});
+      console.log (ret);
+    }
+  });
+});
